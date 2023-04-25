6607cc9136de4e64f25cdfda036614175319e42b
yinheli <me@yinheli.com>
2023-04-25 11:30:20 +0800

    chore: adjust package install

--- debian/server.py
+++ debian/server.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import netaddr
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__, template_folder='./')
 
+packages = [
+  'qemu-guest-agent',
+  'openssh-server',
+  'ca-certificates', 'bash-completion',
+  'file', 'sudo', 'rsync', 'ncdu', 'htop', 'iptables', 'xfsprogs', 'psmisc', 'sysstat', 'apparmor', 'apparmor-utils', 'bsdmainutils',
+  'vim', 'wget', 'curl', 'unzip', 'netcat',
+  'git', 'git-extras',
+  'ethtool', 'telnet', 'traceroute', 'tcpdump', 'iputils-ping',
+  'build-essential', 'python3-dev', 'python3-venv', 'python3-pip',
+  'dnsutils', 'openvpn', 'cifs-utils', 'nfs-common', 'gnupg',
+]
+
+@app.route('/favicon.ico')
+def favicon():
+  return redirect('https://www.debian.org/favicon.ico', code=302)
 
 @app.route('/<host>/<ip>')
 @app.route('/<host>/<ip>/<prefix>')
@@ -15,6 +29,7 @@ def index(host, ip, prefix=24):
     'host': host,
     'addr': addr,
     'gateway': addr.network+1,
+    'packages': ' '.join(packages),
   }
   return render_template('preseed.txt', **params), 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
